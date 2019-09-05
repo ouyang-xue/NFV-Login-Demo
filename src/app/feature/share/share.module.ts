ae02da2e6316b72ed8ac6b723653227096647979
Zhenyi Zhang <a323033@ecitele.com>
2019-09-05 15:58:45 +0800

    add DropdownModule

--- src/app/feature/share/share.module.ts
+++ src/app/feature/share/share.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {ButtonModule} from 'primeng/button';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToolbarModule } from 'primeng/toolbar';
 import {PasswordModule} from 'primeng/password';
 import {InputTextModule} from 'primeng/inputtext';
+import {DropdownModule} from 'primeng/dropdown'; 
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import {InputTextModule} from 'primeng/inputtext';
     ToolbarModule,
     PasswordModule,
     InputTextModule,
-    FormsModule
+    FormsModule,
+    DropdownModule
   ],
   providers: [
   ]
