229efa000a0f94bd2b2ac05ce6b159c72c9a4fa6
Zhenyi Zhang <a323033@ecitele.com>
2019-09-09 21:45:48 +0800

    Handling 401 and 403 error

--- src/app/services/global.interceptor.ts
+++ src/app/services/global.interceptor.ts
@@ -1,24 +1,46 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 
 @Injectable()
 export class GlobalInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let httpReq: HttpRequest<any> = req;
 
-        console.log("##################");
+        console.log("##################  GlobalInterceptor ");
         
 
         const token = this.authService.getAuthorizationToken();
         if(token) {
             httpReq = req.clone( {headers: req.headers.set('X-Access-Token', token)});
         }
-        return next.handle(httpReq);
+        return next.handle(httpReq).pipe(
+            catchError((err, caught) => {
+                console.log(err);
+                return this.handleAuthError(err);
+            })
+        ) as any;
+    }
+
+    private handleAuthError(err: HttpErrorResponse): Observable<any> {
+        // handle your auth error or rethrow
+        if (err.status === 401 || err.status === 403) { // 401: Unauthorized; 403: Forbiden
+            // navigate /delete cookies or whatever
+            this.authService.removeAuthorizationToken();
+            this.router.navigateByUrl(`/login`);
+            // if you've caught / handled the error, you don't want to rethrow it 
+            // unless you also want downstream consumers to have to handle it as well.
+            return of(err.message);
+        }
+        return Observable.throw(err);
     }
     
 }
