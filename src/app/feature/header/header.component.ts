00c5dba7fbf9707dd272d1d16abdbafd29ec88de
Zhenyi Zhang <a323033@ecitele.com>
2019-09-11 09:44:10 +0800

    add Username on header component

--- src/app/feature/header/header.component.ts
+++ src/app/feature/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { UserService } from 'src/app/services/user.service';
+import { GlobalVariable } from 'src/app/common/globals';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +12,18 @@ export class HeaderComponent implements OnInit {
 
   @Input() title: any = "title";
 
+  username: string = "";
+
   constructor(
-    private loginService: LoginService
+    private loginService: LoginService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
+    const userId = window.localStorage.getItem(GlobalVariable.userId);
+    if(userId) {
+      this.userService.getUserById(userId).subscribe(user => this.username = user.username);
+    }
   }
 
   logout() {
