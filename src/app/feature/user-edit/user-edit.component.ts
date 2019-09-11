dd5a4ebc1a865549954f1fdc4cc17b29ff873b8d
liuzhen <Zhen.Liu@ecitele.com>
2019-09-11 12:53:36 +0800

    modify edit-user

--- src/app/feature/user-edit/user-edit.component.ts
+++ src/app/feature/user-edit/user-edit.component.ts
@@ -16,7 +16,7 @@ export class UserEditComponent implements OnInit {
   public psdMinLen: number = 4;
   public psdMaxLen: number = 16;
   public btnDisplay: number = 0;
-  private id:string = null;
+  public id:string = null;
   private title:string = "add";
   users: any[] = [];
   role: SelectItem[];
@@ -40,9 +40,9 @@ export class UserEditComponent implements OnInit {
     private activeRoute: ActivatedRoute,
     private userService: UserService) {
       this.role = [
-        { label: 'Admin', value: '1' },
-        { label: 'Bussiness', value: '2' },
-        { label: 'User', value: '3' }
+        { label: 'Admin', value: 1 },
+        { label: 'Bussiness', value: 2 },
+        { label: 'User', value: 3 }
       ];
   }
   i: number = 0;
@@ -56,7 +56,7 @@ export class UserEditComponent implements OnInit {
       if(this.id!=null){
         this.labelBtn = "Edit";
         this.btnDisplay = 1;
-        this.userService.getUserById(this.id).subscribe((userdata: any) => {
+        this.userService.getUserById(this.id).subscribe(userdata => {
           this.user = userdata;
         })
       }else{
@@ -67,25 +67,19 @@ export class UserEditComponent implements OnInit {
 
   submit() {
     if (this.user._id == null) {
-      
       this.addUser.pwd = Md5.hashStr(this.user.pwd).toString();
       this.addUser.fullname = this.user.fullname;
       this.addUser.username = this.user.username;
       this.addUser.role = this.user.role;
-      console.log(this.addUser);
         this.userService.addUser(this.addUser).subscribe(userData => { 
           this.router.navigate(["/users"]);
         },
         (error: any) => {
-          console.log("---------------------" + error);
-          
           this.warningMsg = [];
           this.warningMsg.push({severity:'warn', summary:'Warn Message: ', detail:error.error.error});
         }
         );
     } else {
-      console.log(this.user);
-      
       this.userService.editUser(this.user).subscribe(userData => {
         this.user = userData;
         this.router.navigate(["/users"]);
